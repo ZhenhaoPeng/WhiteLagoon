984f2c83bf377fc44aac912931a82ca4b02f96af
Zhenhao Peng <pengzhenhao119@gmail.com>
2025-04-12 23:43:04 +0200

    Home index

---
+++ WhiteLagoon.Web/WhiteLagoon.Web/Models/ViewModels/HomeVM.cs
@@ -0,0 +1,13 @@
+﻿using WhiteLagoon.Domain.Entities;
+
+namespace WhiteLagoon.Web.Models.ViewModels
+{
+    public class HomeVM
+    {
+        public IEnumerable<Villa>? VillaList { get; set; }
+        public DateOnly CheckInDate { get; set; }
+        public DateOnly? CheckOutDate { get; set; }
+        public int Nights { get; set; }
+
+    }
+}
--- WhiteLagoon.Web/WhiteLagoon.Web/obj/Debug/net8.0/WhiteLagoon.Web.AssemblyInfo.cs
+++ WhiteLagoon.Web/WhiteLagoon.Web/obj/Debug/net8.0/WhiteLagoon.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WhiteLagoon.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+058412e36705d04fa377b3543fa2de06b538773d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84d9d8a202fa8c6474defd44a8588a16fc2a0c1e")]
 [assembly: System.Reflection.AssemblyProductAttribute("WhiteLagoon.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WhiteLagoon.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
